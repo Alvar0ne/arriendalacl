976b9a344929644ae30a74dea465606093cbcb57
Alvar0ne <alvaro_utem@hotmail.com>
2017-04-13 15:53:37 -0300

    Agregando navbar

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
    root to: 'pages#home'
 
 
+get 'about' => 'pages#about', as: :about
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
