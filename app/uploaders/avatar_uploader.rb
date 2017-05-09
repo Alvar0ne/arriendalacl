abd2eba59d39b21f277ebae9482fd2c03728a257
Alvaro <elxalvaro@gmail.com>
2017-05-08 23:14:41 -0300

    show java

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -8,7 +8,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
-    storage :fog
+  storage :fog
   else
   storage :file
   end
@@ -27,6 +27,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
+
+
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
