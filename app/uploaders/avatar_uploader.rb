2d17b30e39d1f7cb7192b0b9c49be26bb551c5e5
Alvaro <elxalvaro@gmail.com>
2017-05-11 17:56:10 -0300

    Show inmueble

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -47,6 +47,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [200, 200]
   end
 
+   version :large do
+    process :resize_to_limit => [700, 1000]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
